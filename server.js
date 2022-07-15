dcb7bf96d378fb0707ae754a5dd8b959771e568a
Schleimfresse <linuslinuxgenz@gmail.com>
2022-07-15 15:08:28 +0200

    initial

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { request, response } = require("express");
 const app = express();
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 const http = require("http").Server(app);
 server = http.listen(3000, function () {
